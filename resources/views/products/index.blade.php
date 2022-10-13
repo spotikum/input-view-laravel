2b704183f34f79fda1edf95d7925e771e1a9053d
Indra Wahyu <spot@tracert.id>
2022-10-13 23:21:00 +0800

    view

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -20,26 +20,12 @@
             <th>No</th>
             <th>Nama</th>
             <th>Detail</th>
-            <th width="280px">Action</th>
         </tr>
         @foreach ($products as $product)
         <tr>
             <td>{{ ++$i }}</td>
             <td>{{ $product->name }}</td>
             <td>{{ $product->detail }}</td>
-            <td>
-                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
-   
-                    <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
-    
-                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
-   
-                    @csrf
-                    @method('DELETE')
-      
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-            </td>
         </tr>
         @endforeach
     </table>
